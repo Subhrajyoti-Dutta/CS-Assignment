4187d936a0071c383af8b988f58b3f02a66c8b88
Subhrajyoti-Dutta <duttajyotisubhra@gmail.com>
2021-09-08 23:18:11 +0530

    Q3 done

---
+++ Week1/Ques3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+    //Eq1: 2x + 3y = 6
+    //Eq2: 4x + 9y = 15
+    float p1 = 2, q1 = 3, c1 = 6;
+    float p2 = 4, q2 = 9, c2 = 15;
+
+    float x, y;
+
+    x = (c1 * q2 - c2 * q1) / (p1 * q2 - p2 * q1);
+    y = -(c1 * p2 - c2 * p1) / (p1 * q2 - p2 * q1);
+
+    printf("Value of x is: %lf\n", x);
+    printf("Value of y is: %lf", y);
+
+
+    return 0;
+}
