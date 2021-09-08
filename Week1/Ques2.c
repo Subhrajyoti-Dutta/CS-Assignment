b47d7baebc6c578424d90b0538af1a1f24f0e443
Subhrajyoti-Dutta <duttajyotisubhra@gmail.com>
2021-09-08 21:42:03 +0530

    ques2

--- Week1/Ques2.c
+++ Week1/Ques2.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
+int main(int argc, char** argv) {
+    double a, b, c;
+    double sol1, sol2;
+    printf("Write the coefficient of x^2: ");
+    scanf("%lf", &a);
+    printf("Write the coefficient of x: ");
+    scanf("%lf", &b);
+    printf("Write the value of constant term: ");
+    scanf("%lf", &c);
+    sol1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
+    sol2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
+    printf("Solution 1: %lf\n", sol1);
+    printf("Solution 2: %lf", sol2);
+    return 0;
+}
