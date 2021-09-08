71d716bd56370f80f7dd470ad8c7809c9e395dc4
Subhrajyoti-Dutta <duttajyotisubhra@gmail.com>
2021-09-08 21:56:17 +0530

    q1

--- Week1/Ques1.c
+++ Week1/Ques1.c
@@ -3,9 +3,9 @@
 #include <math.h>
 
 int main(int argc, char** argv) {
-    //approximate values of 1/3: a) 0.3 b) 0.32 c) 0.33333
-    double app[3] = { 0.3, 0.32, 0.33333 };
-    double act_val = 1.0 / 3.0, app_val;
+    //approximate values of 1/3: a) 0.7 b) 0.67 c) 0.66667
+    double app[3] = { 0.7, 0.67, 0.66667 };
+    double act_val = 2.0 / 3.0, app_val;
     double abs_err, rel_err, per_err;
     for (int i = 0; i < 3; i++) {
         app_val = app[i];
