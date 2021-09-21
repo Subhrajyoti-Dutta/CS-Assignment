e9e52708931ddcc61294a4b852622535fc1869d8
Subhrajyoti-Dutta <duttajyotisubhra@gmail.com>
2021-09-21 14:20:25 +0530

    Q4

---
+++ Week2/Ques4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+double f(double x) {
+    return x - cos(x);                  //The function whose roots we have to find
+}
+
+int main() {
+    double a, b, epsilon;
+
+    printf("Starting Point 1: ");
+    scanf("%lf", &a);
+    printf("Starting Point 2: ");
+    scanf("%lf", &b);
+    printf("Limit of accuracy: ");
+    scanf("%lf", &epsilon);
+
+    double c = a, fa, fb;
+    fa = f(a);
+    fb = f(b);
+    double fc = fa;
+    do {
+        a = c;
+        fa = fc;
+        c = (fa * b - a * fb) / (fa - fb);
+        fc = f(c);
+    } while (fc != 0 && fabs(fc - fa) > epsilon && fabs(c - a) > epsilon);
+    printf("The root of the function for the desired level of accuracy is: %lf\n", c);
+    return 0;
+}
