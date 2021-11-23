926dc7365276f501d948c2cadfeede3742310c6e
Subhrajyoti Dutta <duttajyotisubhra@gmail.com>
2021-11-23 19:49:49 +0530

    W7Q1 done

---
+++ Week7/Ques2.c
@@ -0,0 +1,13 @@
+//Name: Subhrajyoti Dutta
+//Roll No: 20220STA066
+//Sub: CS-103P
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+	double func(double x, double y) {
+		return x * (y + 1);
+	}
+}
